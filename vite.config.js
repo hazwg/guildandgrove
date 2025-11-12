ddda967b25492c2669507aa04e5e0e1a98dae8ce
hazwg <harry.gott@beaumontventures.co.uk>
2025-11-12 23:38:07 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// If you deploy to https://<user>.github.io/guild-and-grove/ keep base as below.
-// If you use a custom domain or user site, change to base: '/'
 export default defineConfig({
   plugins: [react()],
   base: '/guildandgrove/',
